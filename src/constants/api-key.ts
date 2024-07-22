65a2ad3cfb4b8ce765c2532d7ebc2814255b4078
manhpxrabiloo <manhpx@rabiloo.com>
2024-07-22 00:34:27 -0700

    hidden key youtube api

--- src/constants/api-key.ts
+++ src/constants/api-key.ts
@@ -1,2 +1,2 @@
-export const KEY_YOUTUBE_API = 'AIzaSyDLR1vdce3GHH5shro-DX1yyiweqf-tl8w';
+export const KEY_YOUTUBE_API = '';
 export const BASE_YOUTUBE_URL_API = 'https://www.googleapis.com/youtube/v3';
