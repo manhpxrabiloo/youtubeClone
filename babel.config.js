770925dca3c351c83c6c3587e6c5b101f9e80aba
manhpxrabiloo <manhpx@rabiloo.com>
2024-07-22 00:45:11 -0700

    use env file

--- babel.config.js
+++ babel.config.js
@@ -21,5 +21,13 @@ module.exports = {
         },
       },
     ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
   ],
 };
--- src/constants/api-key.ts
+++ src/constants/api-key.ts
@@ -1,2 +1,4 @@
-export const KEY_YOUTUBE_API = '';
+import {YOUTUBE_API_KEY} from '@env';
+
+export const KEY_YOUTUBE_API = YOUTUBE_API_KEY;
 export const BASE_YOUTUBE_URL_API = 'https://www.googleapis.com/youtube/v3';
